0a7a64d620f9cb8dfa4240b045646d2b937499e2
Rhafhy Al-Farisy Setiawan <rhafhyalfarisy@gmail.com>
2024-09-27 09:49:11 +0700

    si akang jahat bet

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        
+        System.out.println("Nama saya siapa");
+        System.out.println("saya jelek kata kang Nashir");
     }
 }
 
