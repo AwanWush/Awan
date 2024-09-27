7c8d6dbddc92cc478a4cd1e6bfbe1c8c88881005
Rhafhy Al-Farisy Setiawan <rhafhyalfarisy@gmail.com>
2024-09-27 09:53:07 +0700

    remove text line

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Nama saya siapa");
-        System.out.println("saya jelek kata kang Nashir");
     }
 }
 
